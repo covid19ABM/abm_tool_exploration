58399707d5119db7c5996ee129feb2aa061c8d25
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-03-09 16:23:27 +0100

    prototyping agents

--- prototype/agent.py
+++ prototype/agent.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass, fields
 import numpy as np
+import random
 
 @dataclass
 class Features():
     ''' Features of the agents '''
-    
     __slots__ = ['name', 'gender', 'age', 'education', 'employed',
                  'partnership_status', 'pre_existing_depression',
                  'pre_existing_burnout', 'pre_existing_addiction',
@@ -38,7 +38,6 @@ class Features():
 @dataclass
 class Agent():
     ''' Represents an agent in our small world '''
-    
     features: Features
     n_contacts: int = 0
     
@@ -54,19 +53,19 @@ def generate_gender_distribution(n_people, percentage):
     n_people : int
         size of the population.
     percentage : int
-        ratio of male over female
+        ratio of male over female (e.g., 50 for 50%)
 
     Returns
     -------
     gender_distribution : numpy array
-        Array of 1s and 0s (1 = female, 0 = male) 
+        Array of 1s and 0s (1 = female, 0 = male) whose proportions depend on `percentage`.
     '''
     gender_distribution = np.ones(n_people)
     gender_distribution[:int((n_people * percentage) / 100)] = 0
     np.random.shuffle(gender_distribution)       
     return gender_distribution.astype(int)
 
-def generate_age_distribution(n_people, n_age_groups, start, stop, percentage):
+def generate_age_distribution(n_people, n_age_groups, start, stop, prob):
     ''' Generates age distribution
     Parameters
     ----------
@@ -78,12 +77,13 @@ def generate_age_distribution(n_people, n_age_groups, start, stop, percentage):
         Maximum age.
     n_age_groups : int
         Number of age groups.
-    percentage : list    
-        Proportion of age prevalence for each age group. Number of elements in the list must match value in `n_age_groups`
+    prob : list    
+        Proportion of age prevalence for each age group (e.g., .25). Number of elements must match value in `n_age_groups`.
         
     Returns
     -------
     age_distribution : numpy array
+        Array of age distribution in the population. Proportions depend on `prob`.
     '''
 
     age_values = np.arange(start, stop)  
@@ -91,6 +91,34 @@ def generate_age_distribution(n_people, n_age_groups, start, stop, percentage):
 
     age_distribution = []
     for x in range(n_people):
-        idx = np.random.choice(range(len(age_groups)), p=percentage)
+        idx = np.random.choice(range(len(age_groups)), p=prob)
         age_distribution.append(np.random.choice(age_groups[idx]))
     return age_distribution
+
+
+def generate_educational_attainment_distribution(n_people, low, medium, high):
+    ''' Generate distribution of high, medium and low educational attainments in the population.
+    Parameters
+    ----------
+    n_people : int
+        Size of the population.
+    low : float
+        Probability of low educational attainment prevalence in the population. 
+    medium : float
+        Probability of medium educational attainment prevalence in the population. 
+    high : float
+        Probability of high educational attainment prevalence in the population. 
+        
+    Returns
+    ----------
+    educational_attainment : list
+        List of type of educational attainments in the population.
+    '''
+    options = {'Low' : low, 
+               'Medium' : medium,
+               'High' : high}
+    
+    choices = list(options.keys())
+    weights = list(options.values())
+    educational_attainment = random.choices(choices, weights, k = n_people) 
+    return(educational_attainment)
